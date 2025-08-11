598a7dfcf98a227c4e5929eb4aa939dcb9172110
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-11 10:33:37 -0600

    Feat: Added Rentaltem Class

---
+++ Models/RentalItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageRentalManagementSystem.Models
+{
+    internal class RentalItem
+    {
+        public Equipment equipment;
+        public DateTime rentalDate;
+        public DateTime returnDAte;
+        public double rentalCost;
+
+    }
+}
