cfa5cc12577460826f4a332a0801fa1ed91644d9
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-11 10:25:05 -0600

    Feat: Created Equipment Class

---
+++ Models/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VillageRentalManagementSystem.Components.Models;
+
+namespace VillageRentalManagementSystem.Models
+{
+    internal class Equipment
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double DailyRentalCost { get; set; }
+        public bool isAvailable { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
