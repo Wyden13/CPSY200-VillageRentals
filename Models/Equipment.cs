b3f5fee043bacf4aa7ffa2abe3813d333b6b7fe0
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-11 10:42:44 -0600

    Feat: Added Customer class

--- Models/Equipment.cs
+++ Models/Equipment.cs
@@ -13,7 +13,7 @@ namespace VillageRentalManagementSystem.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public double DailyRentalCost { get; set; }
-        public bool isAvailable { get; set; }
+        public bool IsAvailable { get; set; }
         public Category Category { get; set; }
 
     }
---
+++ Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VillageRentalManagementSystem.Components.Models;
+
+namespace VillageRentalManagementSystem.Models
+{
+    internal class Rental
+    {
+        public int Id { get; set; }
+        public Customer customer;
+        
+        List<RentalItem> items;
+    }
+}
