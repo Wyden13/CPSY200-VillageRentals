f8cc9ba754e9b3674b933e33cb976d862690430f
Wendy <128701096+Wyden13@users.noreply.github.com>
2025-08-11 10:00:48 -0600

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VillageRentalManagementSystem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
