9fa8fac45259f32e32e6c01afed610cb59557828
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-12 07:46:22 -0600

    Feat: Add appsettings with Connection String File

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -9,7 +9,7 @@ namespace VillageRentalManagementSystem.Services
 {
     internal class CustomerService
     {
-        List<Customer> customers = new List<Customer>();
+        public List<Customer> customers = new List<Customer>();
 
         public void AddCustomer(Customer customer) { customers.Add(customer); }
         public void RemoveCustomer(Customer customer) {customers.Remove(customer); }
